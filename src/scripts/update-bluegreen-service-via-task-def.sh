638f4db51f271d7e1035c90601440e551e2de0c2
Mary Rose Quito <mary-rose@blinkist.com>
2021-06-14 09:53:36 +0200

    remove unnecessary quotes

--- src/scripts/update-bluegreen-service-via-task-def.sh
+++ src/scripts/update-bluegreen-service-via-task-def.sh
@@ -14,13 +14,13 @@ else
 fi
 
 
-DEPLOYED_REVISION="${CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN}"
+DEPLOYED_REVISION=$CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN
 
 
 DEPLOYMENT_ID=$(aws deploy create-deployment \
-    --application-name "$ECS_PARAM_CD_APP_NAME" \
-    --deployment-group-name "$ECS_PARAM_CD_DEPLOY_GROUP_NAME" \
-    "$DEPLOYMENT_CONFIG_NAME_ARG" \
+    --application-name $ECS_PARAM_CD_APP_NAME \
+    --deployment-group-name $ECS_PARAM_CD_DEPLOY_GROUP_NAME \
+    $DEPLOYMENT_CONFIG_NAME_ARG \
     --revision "{\"revisionType\": \"AppSpecContent\", \"appSpecContent\": {\"content\": \"{\\\"version\\\": 1, \\\"Resources\\\": [{\\\"TargetService\\\": {\\\"Type\\\": \\\"AWS::ECS::Service\\\", \\\"Properties\\\": {\\\"TaskDefinition\\\": \\\"${CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN}\\\", \\\"LoadBalancerInfo\\\": {\\\"ContainerName\\\": \\\"$ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_NAME\\\", \\\"ContainerPort\\\": $ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_PORT}}}}]}\"}}" \
     --query deploymentId \
     --output text)
@@ -28,7 +28,7 @@ echo "Created CodeDeploy deployment: $DEPLOYMENT_ID"
 
 if [ "$ECS_PARAM_VERIFY_REV_DEPLOY" == "1" ]; then
     echo "Waiting for deployment to succeed."
-    if $(aws deploy wait deployment-successful --deployment-id "${DEPLOYMENT_ID}"); then
+    if $(aws deploy wait deployment-successful --deployment-id $DEPLOYMENT_ID); then
         echo "Deployment succeeded."
     else
         echo "Deployment failed."
